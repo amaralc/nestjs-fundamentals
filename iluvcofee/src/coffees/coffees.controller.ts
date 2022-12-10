5968fd7b1f0084a312b7276d5a841966236a27cb
amaralc <amaral.calil@gmail.com>
2022-12-10 17:25:55 -0300

    fix: reset controller methods and use nest default responses

--- iluvcofee/src/coffees/coffees.controller.ts
+++ iluvcofee/src/coffees/coffees.controller.ts
@@ -1,20 +1,10 @@
-import {
-  Body,
-  Controller,
-  Get,
-  HttpCode,
-  HttpStatus,
-  Param,
-  Post,
-  Res,
-} from '@nestjs/common';
-import { Response } from 'express';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get('flavors')
-  findAll(@Res() response: Response) {
-    response.status(200).send('This action returns all coffees');
+  findAll() {
+    return 'This action returns all coffees';
   }
 
   @Get(':id')
@@ -23,7 +13,6 @@ export class CoffeesController {
   }
 
   @Post()
-  @HttpCode(HttpStatus.GONE) // Or @HttpCode(410)
   // It is possible to pass a string to @Body decorater to get only part of te body. @see: https://learn.nestjs.com/courses/591712/lectures/18346817
   create(@Body() body) {
     return body;
