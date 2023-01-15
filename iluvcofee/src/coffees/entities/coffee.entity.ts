34cd153b22fb7693c5d58a5949d91beba0d83e46
amaralc <amaral.calil@gmail.com>
2023-01-15 14:23:20 -0300

    feat: add many to many relation between coffee and flavor

--- iluvcofee/src/coffees/entities/coffee.entity.ts
+++ iluvcofee/src/coffees/entities/coffee.entity.ts
@@ -1,4 +1,11 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Flavor } from './flavor.entity';
 
 /**
  * The default sql table generated is the lower case version of the class name, 'coffee',
@@ -19,7 +26,20 @@ export class Coffee {
   @Column()
   brand: string;
 
-  // Inform typeorm to generate a nullable column with json values (temporary, soon we will create relationships)
-  @Column('json', { nullable: true })
+  // // Inform typeorm to generate a nullable column with json values (temporary, soon we will create relationships)
+  // @Column('json', { nullable: true })
+  // flavors: string[];
+
+  /**
+   * Decorator registered only in the owner of the relation
+   */
+  @JoinTable()
+  /**
+   * ManyToMany parameters
+   *
+   * 1 - Relation type: Function that returns a reference to the related entity
+   * 2 - Arrow function that returns the related entity and specifies what property needs to be selected that is the inverse side of the relationship (what is coffee inside of the Flavors entity?)
+   */
+  @ManyToMany((type) => Flavor, (flavor) => flavor.coffees)
   flavors: string[];
 }
